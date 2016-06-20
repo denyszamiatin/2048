776f2dbfdb8584b0e0a618f10804904a09e1d2ab
Marina <frontline.fraulein@gmail.com>
2016-06-20 19:41:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 EMPTY_CELL = 0
 DIMENSION = 4
-NEW_CELL1 = 2
-NEW_CELL2 = 4
+NEW_CELL_IS_2 = 2
+NEW_CELL_IS_4 = 4
 
 def create_field():
     '''
@@ -223,6 +223,6 @@ def return_string(field, string, x):
 def generate_number(a=1, b=4):
     number = random.randint(a, b)
     if number < 4:
-        return NEW_CELL1
+        return NEW_CELL_IS_2
     else:
-        return NEW_CELL2
+        return NEW_CELL_IS_4
